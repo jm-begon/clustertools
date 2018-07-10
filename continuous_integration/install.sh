7744f00feffe9f4786a8071c1c128cc5004cc7a0
jm-begon <jm.begon@ulg.ac.be>
2018-07-10 10:54:33 +0200

    ENH testing on Slurm

---
+++ continuous_integration/install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# This script is meant to be called by the "install" step defined in
+# .travis.yml. See http://docs.travis-ci.com/ for more details.
+# The behavior of the script is controlled by environment variabled defined
+# in the .travis.yml in the top level folder of the project.
+
+# License: 3-clause BSD
+
+
+set -xe # Exit on first error
+
+
+if [[ "$SCHEDULER" == "SLURM" ]]; then
+    sudo apt-get install slurm-llnl
+    sudo /usr/sbin/create-munge-key
+    sudo service munge start
+    sudo python continuous_integration/configure_slurm.py
+fi
